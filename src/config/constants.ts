5107d34bb13b75faa1d111cdb06c326b293dd301
PandaDev03 <daiphucduongvinh203@gmail.com>
2025-12-12 15:26:43 +0700

    refactor(constants): Move constants to shared/constants and add URL fields

--- src/config/constants.ts
+++
@@ -1,6 +0,0 @@
-const SIZES = ['s', 'm', 'l', 'xl'] as const;
-
-const MIN_QUANTITY = 1;
-const MAX_QUANTITY = 99;
-
-export { SIZES, MIN_QUANTITY, MAX_QUANTITY };
---
+++ src/shared/utils/constants.ts
@@ -0,0 +1,8 @@
+const SIZES = ['s', 'm', 'l', 'xl'] as const;
+
+const MIN_QUANTITY = 1;
+const MAX_QUANTITY = 99;
+
+const DEFAULT_URL_FIELDS = ['website', 'facebook', 'instagram'];
+
+export { SIZES, MIN_QUANTITY, MAX_QUANTITY, DEFAULT_URL_FIELDS };
