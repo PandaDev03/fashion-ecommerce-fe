e8b0f9944136ec3f705964d9ed0fbaead7601b73
PandaDev03 <daiphucduongvinh203@gmail.com>
2025-12-25 21:12:45 +0700

    feat(layout): make sidebar menu dynamic and fix active state for order routes

--- src/layouts/AdminLayout/components/Sidebar/Sidebar.tsx
+++ src/layouts/AdminLayout/components/Sidebar/Sidebar.tsx
@@ -54,7 +54,11 @@ const routePatterns = [
   },
   {
     pattern: PATH.ADMIN_ORDER_MANAGEMENT,
-    keys: [PATH.ADMIN_ORDER_MANAGEMENT, PATH.ADMIN_ORDER_DETAILS],
+    keys: [PATH.ADMIN_ORDER_MANAGEMENT],
+  },
+  {
+    pattern: PATH.ADMIN_ORDER_DETAILS,
+    keys: [PATH.ADMIN_ORDER_MANAGEMENT],
   },
 ];
 
@@ -87,7 +91,7 @@ const Sidebar = ({ className, ...props }: SiderProps) => {
           mode="inline"
           items={siderItems}
           className="h-full"
-          defaultSelectedKeys={['1']}
+          defaultSelectedKeys={[PATH.ADMIN_DASHBOARD]}
           selectedKeys={getSelectedKey(location.pathname)}
           defaultOpenKeys={location?.state?.key ? [location?.state?.key] : []}
           onSelect={(e) =>
